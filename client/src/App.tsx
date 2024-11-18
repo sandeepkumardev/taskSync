76ced80af9c7b866da8675fe6f20e935cae84508
sandeep kumar <sandeepypb@gmail.com>
2024-11-18 08:43:21 +0530

    ✨ Feat: add components and theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Admin from "./pages/Admin";
 import User from "./pages/User";
 import NotFound from "./pages/NotFound";
+import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Admin />} />
-        <Route path="/room/:roomNumber" element={<User />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Admin />} />
+          <Route path="/room/:roomNumber" element={<User />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
