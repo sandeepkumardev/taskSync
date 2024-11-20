f80f3ec59401edb5f79f9cf43612a240e2b8d5a6
sandeep kumar <sandeepypb@gmail.com>
2024-11-20 21:48:39 +0530

    🐛 Fix: remove unused toast

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,16 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { toast as shadcnToast } from "@/hooks/use-toast";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const toast = (message = "", type = "default", duration = 3000) => {
-  return shadcnToast({
-    description: message,
-    variant: type === "error" ? "destructive" : "default",
-    duration,
-    title: type === "error" ? "Error" : "Success",
-  });
-};
